34e174724eefba4917702780f5b1ba4e222a0649
Rekkus <kays_zuhair@hotmail.com>
2022-04-26 17:54:31 +0200

    Izi code

---
+++ belief-base.py
@@ -0,0 +1,21 @@
+from sympy import *
+
+
+
+
+class Stuff:
+    def __init__(self):
+        self.something = 0
+
+
+    def to_cnf():
+        letters = input()
+        conv = to_cnf(letters)
+        print(conv)
+
+
+
+
+
+
+Stuff.to_cnf()
--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # -*- coding: utf-8 -*-
+from sympy import to_cnf
+from belief-base import Stuff
+
+
+
+def inputs(BB):
+    print('Write input:')
+    action = input()
+    form = to_cnf(action)
+
+
 
